f8777f72c5414df66d071122157df136efc53c75
ciscoducasse99 <ciscoducasse99@hotmail.com>
2022-02-11 18:58:33 -0500

    migrate from cleardb to jawsdb

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const path = require("path");
 // IIFE
 (async () => {
   try {
-    console.log(knex.client.connectionSettings);
     // Acquiring Port to run server
     const { APP_PORT } = process.env;
     const port = process.env.PORT || APP_PORT;
